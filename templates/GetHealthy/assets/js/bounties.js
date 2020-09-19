103a01f86fae7cad7e4231f99a8cc21da32e74d6
shachindra <shachindra.spidey@gmail.com>
2020-09-19 19:16:43 +0530

    starter screens

---
+++ templates/GetHealthy/assets/js/bounties.js
@@ -0,0 +1,24 @@
+$(function() {
+
+    // Read User Info to show the bounties
+    $.getJSON("./dataset/user.json", function(data) {
+        console.log(JSON.stringify(data));
+        $('#category').text(data.category);
+    }).fail(function() {
+        console.log("An error has occurred.");
+    });
+
+    // Read User Info to show the bounties
+    $.getJSON("./dataset/bounties.json", function(data) {
+        console.log(data.length);
+        $.each(data, function(key, val) {
+            if (val.category === $('#category').text()) {
+                let element = "<div class='card'><div class='content'><h2 class='title'>" + val.title + "</h2><p class='copy'>" + val.content + "</p><p class='copy'>" + val.rewards + "</p><button class='btn'>Claim</button></div></div>";
+                $('.page-content').append(element);
+            }
+        });
+    }).fail(function() {
+        console.log("An error has occurred.");
+    });
+
+});
